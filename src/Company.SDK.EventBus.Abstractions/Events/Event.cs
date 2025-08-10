79a9a10f3f820a368c4018478ec4d5b18f45f843
Rafael Deroncio <rafael.deroncio@hotmail.com>
2025-08-09 22:40:51 -0300

    melhorias no event base

--- src/Company.SDK.EventBus.Abstractions/Events/Event.cs
+++ src/Company.SDK.EventBus.Abstractions/Events/Event.cs
@@ -4,19 +4,46 @@ namespace Company.SDK.EventBus.Abstractions.Events;
 
 public class Event : IDisposable
 {
-    [JsonIgnore]
-    public string EventId { get; } = Guid.NewGuid().ToString("N");
+    public string EventId => _eventId;
 
-    [JsonIgnore]
-    public DateTime CreationDate { get; } = DateTime.UtcNow;
+    public DateTime CreationDate => _creationDate;
 
-    [JsonIgnore]
+    public int ReceiveCount => _receiveCount;
+
     public string ErrorMessage { get; set; } = string.Empty;
-
-    [JsonIgnore]
-    public int ReceiveCount { get; set; } = 0;
 
     public void Dispose() => GC.SuppressFinalize(this);
 
     ~Event() => Dispose();
+
+    private string _eventId = Guid.NewGuid().ToString("N");
+
+    private DateTime _creationDate = DateTime.UtcNow;
+
+    private int _receiveCount = 0;
+
+    public Event SetCreationDate(DateTime creationDate)
+    {
+        if (creationDate != default)
+            _creationDate = creationDate;
+
+        _creationDate = creationDate;
+        return this;
+    }
+
+    public Event SetEventId(string eventId)
+    {
+        if (!string.IsNullOrWhiteSpace(eventId))
+            _eventId = eventId;
+
+        return this;
+    }
+    
+    public Event SetReceiveCount(int receiveCount)
+    {
+        if (receiveCount >= 0)
+            _receiveCount = receiveCount;
+
+        return this;
+    }
 }
